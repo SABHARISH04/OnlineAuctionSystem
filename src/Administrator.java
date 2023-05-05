e469f5b3ea909da14f10a972a29541da812fa9fb
Sona <118504859+S-o-n-a@users.noreply.github.com>
2023-05-05 21:17:24 +0530

    Update Administrator.java

--- src/Administrator.java
+++ src/Administrator.java
@@ -31,6 +31,68 @@ public class Administrator extends Manager {
         tableData();
     }
 
+public Administrator() {
+    startAuctionButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            startTimer();
+            timer.start();
+
+        }
+    });
+    closeButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            aframe.dispose();
+
+        }
+    });
+
+}
+
+    public void startTimer(){
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sec--;
+                if (sec==-1){
+                    timer.stop();
+                    tableData();
+                }
+                else if(sec>=0&&sec<10)timerLabel.setText("00:0"+sec);
+                else timerLabel.setText("00:"+sec);
+            }
+        });
+table1.addMouseListener(new MouseAdapter() {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        DefaultTableModel dm =(DefaultTableModel) table1.getModel();
+        int selectedRow = table1.getSelectedRow();
+        adminNameData=dm.getValueAt(selectedRow,0).toString();
+        adminPriceData=dm.getValueAt(selectedRow,2).toString();
+        byte[] img = (byte[]) dm.getValueAt(selectedRow,1);
+        ImageIcon imageIcon = new ImageIcon(img);
+        Image im = imageIcon.getImage();
+        Image newimg = im.getScaledInstance(200,200,Image.SCALE_SMOOTH);
+        ImageIcon finalPic = new ImageIcon(newimg);
+        adminImageData = finalPic;
 
 
 
+
+    }
+});
+        }
+    public void tableData() {
+        try{
+            String a= "Select* from auction";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/auction","root","root");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(a);
+            table1.setModel(Customer.buildTableModel(rs));
+        }catch (Exception ex1){
+            JOptionPane.showMessageDialog(null,ex1.getMessage());
+        }
+    }
+}
